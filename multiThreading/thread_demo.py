1756480eece157808cfafa67a04551def3c884c2
GaNiziolek <ganiziolek@gmail.com>
2020-02-10 13:09:14 -0300

    working

---
+++ multiThreading/thread_demo.py
@@ -0,0 +1,14 @@
+import argparse
+import cv2
+from CountsPerSec import CountsPerSec
+from VideoGet import VideoGet
+from VideoShow import VideoShow
+
+def putIterationsPerSec(frame, iterations_per_sec):
+    """
+    Add iterations per second text to lower-left corner of a frame.
+    """
+
+    cv2.putText(frame, "{:.0f} iterations/sec".format(iterations_per_sec),
+        (10, 450), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255))
+    return frame
